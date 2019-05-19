05c5b322ea3219acd80d272f64310f985111a690
Alexander Kuziv <makklays@gmail.com>
2019-05-19 15:52:06 +0300

    Update reflaction.php

--- reflaction.php
+++ reflaction.php
@@ -1,7 +1,9 @@
 <?php
 
+// Класс Step7plus1 
 class Step7plus1 
 {
+    // Ф-ция getBun
     public function getBun() 
     {
         $bun = 'bun';
@@ -11,5 +13,22 @@ class Step7plus1
 
 $reflection = new ReflectionClass('Step7plus1');
 $method = $reflaction->getMethod('getBun');
-// получаем аннотации к методу класса
+// получаем аннотации к классу и методу класса
+echo $reflection->getDocComment();
 echo $method->getDocComment();
+
+// список методов класса
+$methods = $reflection->getMethods();
+echo '<pre>';
+print_r($methods);
+echo '</pre>';
+
+// список свойств класса 
+$properties = $reflection->getProperties();
+echo '<pre>';
+print_r($properties);
+echo '</pre>';
+foreach($properties as $prop)
+{
+    echo $prop->getName();
+}
